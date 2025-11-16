7aa4c80c2ba3b0b785cb515ec4ed25c910b10cab
Denis T. <deadspell21391@gmail.com>
2025-11-16 15:14:58 +0100

    AssemblyInfo.cs

---
+++ Encryptix Crypter/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Encryptix Crypter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Encryptix by unknown")]
+[assembly: AssemblyProduct("Encryptix Crypter")]
+[assembly: AssemblyCopyright("Copyright © Encryptix 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6BF7281C-70AB-4645-85DA-71FC8FFF88E9")]
+[assembly: AssemblyVersion("2.32.684.1283")]
+[assembly: AssemblyFileVersion("2.32.684.1283")]
